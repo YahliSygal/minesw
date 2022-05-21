bebcc35e18a913c0f054ace0f0da4a248fef6d07
YahliSygal <64043827+YahliSygal@users.noreply.github.com>
2022-05-21 17:49:00 +0300

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -48,7 +48,7 @@ namespace minesweeper
 
         public void OnClick(View v)
         {
-            Intent i = new Intent(this, typeof(Menu));
+            Intent i = new Intent(this, typeof(GameGrid));
             if (v == easy)
             {
                 i.PutExtra("dif", "easy");
